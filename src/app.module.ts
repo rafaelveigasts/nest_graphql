a35bd6a0f1e4d84f587ae29156fff963e4795534
Rafael Veiga <rafaelveig@hotmail.com>
2024-08-12 14:48:16 -0300

    📦 chore(package.json): update dependencies for Apollo Server, NestJS Apollo, NestJS GraphQL, and GraphQL 🚀 feat(app.module.ts): add GraphQLModule configuration for Apollo Server with autoSchemaFile path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './database/database.module'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo'
+import path from 'node:path'
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
