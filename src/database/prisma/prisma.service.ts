b3f0473c5259fda9b6a3e9c7d9523a64c1cb95b7
Rafael Veiga <rafaelveig@hotmail.com>
2024-08-12 14:36:45 -0300

    🚀 feat(prisma.service.ts): extend PrismaClient and implement lifecycle hooks

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  async onModuleInit(): Promise<void> {
+    await this.$connect()
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect()
+  }
+}
